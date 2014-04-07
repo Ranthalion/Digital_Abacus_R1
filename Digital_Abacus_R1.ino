8f6b563895597e73e858b135525a91aed3dba0e1
Ranthalion <Ranthalion@hotmail.com>
2014-04-07 14:25:28 -0400

    Adding Game Mode

--- Digital_Abacus_R1.ino
+++ Digital_Abacus_R1.ino
@@ -2,14 +2,14 @@
 #include <Adafruit_MCP23017.h>
 #include <Adafruit_RGBLCDShield.h>
 
-// These #defines make it easy to set the backlight color
 #define RED 0x1
-#define YELLOW 0x3
 #define GREEN 0x2
-#define TEAL 0x6
-#define BLUE 0x4
-#define VIOLET 0x5
-#define WHITE 0x7
+
+#define YELLOW 0x3 	//AND
+#define BLUE 0x4	//OR
+#define VIOLET 0x5	//XOR
+#define TEAL 0x6	//ASCII
+#define WHITE 0x7 	//display and Menu
 
 #define DISPLAY 0x2
 #define AND 0x3
@@ -18,21 +18,23 @@
 #define ASCII 0x6
 #define MENU 0x7
 
+#define INITIAL 0x0	
+#define REDRAW 	0x1
+#define WAITING 0x2
+#define Incorrect 0x3
+#define Success  0x4
+
 byte currentMode = DISPLAY;
-boolean optionChanged = false;
 byte currentOption = DISPLAY;
+boolean optionChanged = false;
 
-const String menu[] = {"", "",
-                    "DISPLAY",
-                    "AND",
-                    "OR",
-                    "XOR",
-                    "ASCII"};
+byte state = INITIAL;
+int num1;
+int num2;
+int answer;
 
-//An array to receive a binary 8-bit number on not necessarily consecutive pins, ordered by LSb to MSb
+const String menu[] = {"", "", "DISPLAY", "AND", "OR", "XOR", "ASCII"};
 const byte abacusPins[] = {2, 3, 4, 5, 6, 7, 8, 9};
-// Size of the dipPins[] array in number of bytes. (Remember, this is one of the few built in functions that doesn't use camel case...)
-const byte abacusSize = sizeof(abacusPins);  
 
 Adafruit_RGBLCDShield lcd = Adafruit_RGBLCDShield();
 
@@ -49,29 +51,19 @@ void setup(){
   //Initiate LCD
   lcd.begin(16,2);
   lcd.setBacklight(WHITE);
-  lcd.print("     FlipBit");  
-  lcd.setCursor(0,1);
-  lcd.print(" by DREAM TEAM");
+  lcdDisplay("     FlipBit", " by DREAM TEAM");
   delay(1500);
 }
 
 void loop(){
 
-  /*
-    Modes - Numeric Display (Binary, Hex, Dec, ASCII
-          - Game
-            - AND
-            - OR
-            - XOR
-            -
-  */
-  
   uint8_t buttons = lcd.readButtons();
   
   if (buttons & BUTTON_UP)
   {
     Serial.println("Pressed up.");
     lcd.clear();
+	lcd.setBacklight(WHITE)
     currentMode = MENU;
     optionChanged = true;
     currentOption--;    
@@ -83,6 +75,7 @@ void loop(){
   {
     Serial.println("Pressed down.");
     lcd.clear();
+	lcd.setBacklight(WHITE)
     currentMode = MENU;
     optionChanged = true;
     currentOption++;
@@ -100,24 +93,48 @@ void loop(){
       displayNumbers();
       break;
     case AND:
-      displayAND(buttons);
-      break;
     case OR:
-      displayOR(buttons);
-      break;
-    case XOR:
-      displayXOR(buttons);
-      break;
-    case ASCII:
-      displayASCII(buttons);
+    case XOR:      
+    case ASCII:      
+	  runGame(buttons);
       break;
     default:
       break;    
   }
 }
 
+void displayMenu(uint8_t buttons)
+{  
+  if (optionChanged)
+  {
+	optionChanged = false;
+    int next = currentOption+1;
+    if (next > 6)
+      next = 2;    
+	  
+    lcd.clear(); 
+    lcd.setCursor(0,0);
+    lcd.print(">> ");
+    lcd.print(menu[currentOption]);
+    lcd.setCursor(0,1);
+    lcd.print("   ");
+    lcd.print(menu[next]);
+  }
+  if (buttons & BUTTON_SELECT)
+  {
+	currentMode = currentOption;
+	state = 0;
+  }
+}
+
 void displayNumbers()
 {
+  if (state == 0)
+  {
+	state = 1;
+	lcd.setBacklight(WHITE)
+	lcd.clear();
+  }
   byte input = readAbacus();
   
   char line1[16];
@@ -128,65 +145,86 @@ void displayNumbers()
   toBinary(tmp, input);
   sprintf(line1, "0b %8s %4c", tmp, input);
   sprintf(line2, "0x %02X %10d", input, input);
-  //lcd.clear();
-  lcd.setCursor(0,0);
-  lcd.print(line1);
   
-  lcd.setCursor(0,1);
-  lcd.print(line2);   
+  lcdDisplay(line1, line2);
+  
 }
 
-void displayAND(uint8_t buttons)
-{
-  lcd.setCursor(0,0);
-  lcd.print("AND");
-  lcd.setCursor(0,1);
-  lcd.print("Not Implemented.");
-}
 
-void displayOR(uint8_t buttons)
+
+void runGame(uint8_t buttons)
 {
-  lcd.setCursor(0,0);
-  lcd.print("OR");
-  lcd.setCursor(0,1);
-  lcd.print("Not Implemented.");
-}
-void displayXOR(uint8_t buttons)
-{
-  lcd.setCursor(0,0);
-  lcd.print("XOR");
-  lcd.setCursor(0,1);
-  lcd.print("Not Implemented.");
-}
-void displayASCII(uint8_t buttons)
-{
-  lcd.setCursor(0,0);
-  lcd.print("XOR");
-  lcd.setCursor(0,1);
-  lcd.print("Not Implemented.");
-}
-void displayMenu(uint8_t buttons)
-{  
-  if (optionChanged)
+   /* States
+	0 - Initial 	(Create 2 Random numbers and calculated expected Answer) -> REDRAW
+	1 - REDRAW 	(Display the numbers and change the screen color) -> Waiting
+	2 - Waiting 	(Wait for button select to be pressed) -> Wrong, Success
+	3 - Incorrect	(Display Try Again message and change screen color, wait for millis or button click) -> REDRAW
+	4 - Success 	(Display Success message and change screen color) -> Initial
+  */
+  switch (state)
   {
-    char line1[16];
-    char line2[16];
-    
-    optionChanged = false;
-    int next = currentOption+1;
-    if (next > 6)
-      next = 2;    
-    
-    //sprintf(line1, ">> %s", menu[currentOption]);
-    //sprintf(line2, "   %s", menu[next]);
-    
-    lcd.clear(); 
-    lcd.setCursor(0,0);
-    lcd.print(">> ");
-    lcd.print(menu[currentOption]);
-    lcd.setCursor(0,1);
-    lcd.print("   ");
-    lcd.print(menu[next]);
+	case INITIAL:
+		num1 = Rand(255);
+		num2 = Rand(255);
+		switch(currentMode)
+		{
+			case AND:
+				answer = num1 & num2;
+				break;
+			case OR:
+				answer = num1 | num2;
+				break;
+			case XOR:
+				answer = num1 ^ num2;
+				break;
+			default:
+				break;
+		}
+		state = REDRAW;
+		break;
+	case REDRAW:
+		char byte1[9];
+		char byte2[9];
+		char line1[16];
+		char line2[16];
+		
+		toBinary(byte1, num1);
+		toBinary(byte2, num2);
+		
+		sprintf(line1, "    %8s", byte1);
+		sprintf(line2, "%3s %8s", menu[currentMode], byte2);
+		lcd.setBacklight(currentMode);
+		lcd.clear();
+		lcdDisplay(line1, line2);
+		state = WAITING;
+		break;
+	case WAITING:
+		if (buttons & BUTTON_SELECT)
+		{
+			byte input = readAbacus();
+			if (input == answer)
+				state = SUCCESS;
+			else
+				state = INCORRECT;
+			delay(250);
+		}
+		break;
+	case INCORRECT:
+		lcd.setBacklight(RED);
+		lcd.clear();
+		lcdDisplay("   Incorrect", "   Try Again");
+		delay(2000);
+		state = REDRAW;
+		break;
+	case SUCCESS:
+		lcd.setBacklight(GREEN);
+		lcd.clear();
+		lcdDisplay("   Correct!", "");
+		delay(2000);
+		state = INITIAL;
+		break;
+	default:
+		break;	
   }
 }
 
@@ -206,9 +244,6 @@ void toBinary(char* buf, byte val)
   buf[8] = '\0';
 }
 
-
-
-
 byte readAbacus()
 {
   byte val = 0;
@@ -218,4 +253,13 @@ byte readAbacus()
     val |= digitalRead(abacusPins[i]);
   }
   return val;
+}
+
+void lcdDisplay(char* line1, char* line2)
+{
+  lcd.setCursor(0,0);
+  lcd.print(line1);
+  
+  lcd.setCursor(0,1);
+  lcd.print(line2);   
 }
