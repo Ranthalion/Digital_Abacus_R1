b3f1b73a6e07e191d488712f0c76288c9fbe5b1c
Ranthalion <Ranthalion@hotmail.com>
2014-04-06 23:34:37 -0400

    Implementing Menu and Display
    Implementing Menu and Display

--- Digital_Abacus_R1.ino
+++ Digital_Abacus_R1.ino
@@ -1,19 +1,40 @@
-//An array to receive a binary 8-bit number on not necessarily consecutive pins, ordered by LSb to MSb
-const byte abacusPins[] = {22, 24, 26, 28, 30, 32, 34, 36};
+#include <Wire.h>
+#include <Adafruit_MCP23017.h>
+#include <Adafruit_RGBLCDShield.h>
 
+// These #defines make it easy to set the backlight color
+#define RED 0x1
+#define YELLOW 0x3
+#define GREEN 0x2
+#define TEAL 0x6
+#define BLUE 0x4
+#define VIOLET 0x5
+#define WHITE 0x7
+
+#define DISPLAY 0x2
+#define AND 0x3
+#define OR 0x4
+#define XOR 0x5
+#define ASCII 0x6
+#define MENU 0x7
+
+byte currentMode = DISPLAY;
+boolean optionChanged = false;
+byte currentOption = DISPLAY;
+
+const String menu[] = {"", "",
+                    "DISPLAY",
+                    "AND",
+                    "OR",
+                    "XOR",
+                    "ASCII"};
+
+//An array to receive a binary 8-bit number on not necessarily consecutive pins, ordered by LSb to MSb
+const byte abacusPins[] = {2, 3, 4, 5, 6, 7, 8, 9};
 // Size of the dipPins[] array in number of bytes. (Remember, this is one of the few built in functions that doesn't use camel case...)
 const byte abacusSize = sizeof(abacusPins);  
 
-#include <Adafruit_GFX.h>
-#include <Adafruit_PCD8544.h>
-
-// pin 7 - Serial clock out (SCLK)
-// pin 6 - Serial data out (DIN)
-// pin 5 - Data/Command select (D/C)
-// pin 4 - LCD chip select (CS)
-// pin 3 - LCD reset (RST)
-Adafruit_PCD8544 display = Adafruit_PCD8544(7, 6, 5, 4, 3);
-
+Adafruit_RGBLCDShield lcd = Adafruit_RGBLCDShield();
 
 void setup(){
   // Set each pin connected to the Abacus switches as an input
@@ -22,90 +43,179 @@ void setup(){
     pinMode(abacusPins[i], INPUT);
   }
 
-  display.begin();
-  display.clearDisplay();
-  // you can change the contrast around to adapt the display
-  // for the best viewing!
-  display.setContrast(20);
-  display.display();
-
+  //Serial for debugging
+  Serial.begin(9600);
+  
+  //Initiate LCD
+  lcd.begin(16,2);
+  lcd.setBacklight(WHITE);
+  lcd.print("     FlipBit");  
+  lcd.setCursor(0,1);
+  lcd.print(" by DREAM TEAM");
+  delay(1500);
 }
 
 void loop(){
 
-  int binaryNumber = parallelToByte();
-
-  display.clearDisplay();
-  display.setTextSize(1);
-  display.setCursor(10,0);
-  display.setTextColor(WHITE, BLACK); // 'inverted' text
-  display.print("DREAM TEAM");
-
-  display.setTextColor(BLACK);
-  display.setCursor(0,10);
-  display.print(binaryNumber, BIN); 
-  display.setCursor(65, 10);
-  display.print("BIN");
-  // Erase the leading 1 (msb 9th digit) and leave 8 leading zeroes
-  display.setTextColor(WHITE);
-  display.setCursor(0,10); display.print("1");
-  display.setTextColor(BLACK);
+  /*
+    Modes - Numeric Display (Binary, Hex, Dec, ASCII
+          - Game
+            - AND
+            - OR
+            - XOR
+            -
+  */
   
-  display.setCursor(37, 20);
-  display.print(binaryNumber, HEX);
-  // Erase the leading 1 (msb 9th digit) and leave 8 leading zeroes
-  display.setTextColor(WHITE);
-  display.setCursor(37,20); display.print("1");
-  display.setTextColor(BLACK);
-
-  display.setCursor(65, 20);
-  display.print("HEX");
+  uint8_t buttons = lcd.readButtons();
   
-  // I subtract that extra 256 from here so I can get a normal decimal
-  display.setCursor(37, 40);
-  display.print(binaryNumber - 256); 
-  display.setCursor(65, 40);
-  display.print("DEC");
-
-  delay(250);
-
-  //Generate printable ASCII characters in the empty space on screen
-  binaryNumber = binaryNumber - 256;
-  if ((binaryNumber > 32) && (binaryNumber < 127)){
-  char binaryString = binaryNumber;  
-  // itoa(binaryNumber, binaryString, 1);  
-    
-    display.setTextSize(2);
-//      display.setTextColor(WHITE, BLACK); // 'inverted' text
-      display.setCursor(10, 20);
-      display.print(binaryString);
-    
-  }
-  
-  display.display();
-  
-}
-
-
-
-/*
- * parallelToByte() - converts a binary value on 8 digital inputs to an integer.
- */
-int parallelToByte()
-{
-  int ret = 0;  // Initialize the variable to return
-  int bitValue[] = {1,2,4,8,16,32,64,128};  // bit position decimal equivalents
-  for(int i = 0; i < 8; i++)  // cycle through all the pins
+  if (buttons & BUTTON_UP)
   {
-    if(digitalRead(abacusPins[i]) == HIGH)  // because all the bits are pre-loaded with zeros, only need to do something when we need to flip a bit high
-    {
-      ret = ret + bitValue[i];  // adding the bit position decimal equivalent flips that bit position
-    }
+    Serial.println("Pressed up.");
+    lcd.clear();
+    currentMode = MENU;
+    optionChanged = true;
+    currentOption--;    
+    if (currentOption <= 0x1)
+      currentOption = 0x6;
+    delay(250);
   }
-  // I'm adding 256 to the final value to b/c I can't print leading zeros
-  // so I add a 9th digit which is always '1' and then overwriting it
-  // with a '1' in WHITE so it disappears from the screen
-  return ret + 256;
+  else if (buttons & BUTTON_DOWN)
+  {
+    Serial.println("Pressed down.");
+    lcd.clear();
+    currentMode = MENU;
+    optionChanged = true;
+    currentOption++;
+    if (currentOption >= 0x7)
+      currentOption = 0x2;
+    delay(250);
+  }
+  
+  switch(currentMode)
+  {
+    case MENU:
+      displayMenu(buttons);
+      break;
+    case DISPLAY: 
+      displayNumbers();
+      break;
+    case AND:
+      displayAND(buttons);
+      break;
+    case OR:
+      displayOR(buttons);
+      break;
+    case XOR:
+      displayXOR(buttons);
+      break;
+    case ASCII:
+      displayASCII(buttons);
+      break;
+    default:
+      break;    
+  }
+}
+
+void displayNumbers()
+{
+  byte input = readAbacus();
+  
+  char line1[16];
+  char line2[16];
+  char tmp[9];
+  Serial.println(input);
+
+  toBinary(tmp, input);
+  sprintf(line1, "0b %8s %4c", tmp, input);
+  sprintf(line2, "0x %02X %10d", input, input);
+  //lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print(line1);
+  
+  lcd.setCursor(0,1);
+  lcd.print(line2);   
+}
+
+void displayAND(uint8_t buttons)
+{
+  lcd.setCursor(0,0);
+  lcd.print("AND");
+  lcd.setCursor(0,1);
+  lcd.print("Not Implemented.");
+}
+
+void displayOR(uint8_t buttons)
+{
+  lcd.setCursor(0,0);
+  lcd.print("OR");
+  lcd.setCursor(0,1);
+  lcd.print("Not Implemented.");
+}
+void displayXOR(uint8_t buttons)
+{
+  lcd.setCursor(0,0);
+  lcd.print("XOR");
+  lcd.setCursor(0,1);
+  lcd.print("Not Implemented.");
+}
+void displayASCII(uint8_t buttons)
+{
+  lcd.setCursor(0,0);
+  lcd.print("XOR");
+  lcd.setCursor(0,1);
+  lcd.print("Not Implemented.");
+}
+void displayMenu(uint8_t buttons)
+{  
+  if (optionChanged)
+  {
+    char line1[16];
+    char line2[16];
+    
+    optionChanged = false;
+    int next = currentOption+1;
+    if (next > 6)
+      next = 2;    
+    
+    //sprintf(line1, ">> %s", menu[currentOption]);
+    //sprintf(line2, "   %s", menu[next]);
+    
+    lcd.clear(); 
+    lcd.setCursor(0,0);
+    lcd.print(">> ");
+    lcd.print(menu[currentOption]);
+    lcd.setCursor(0,1);
+    lcd.print("   ");
+    lcd.print(menu[next]);
+  }
+}
+
+void toBinary(char* buf, byte val)
+{
+  sprintf(buf, 
+            "%c%c%c%c%c%c%c%c", 
+            (val & 0x80) ? '1':'0', 
+            (val & 0x40) ? '1':'0', 
+            (val & 0x20) ? '1':'0', 
+            (val & 0x10) ? '1':'0', 
+            (val & 0x08) ? '1':'0', 
+            (val & 0x04) ? '1':'0', 
+            (val & 0x02) ? '1':'0', 
+            (val & 0x01) ? '1':'0');
+  
+  buf[8] = '\0';
 }
 
 
+
+
+byte readAbacus()
+{
+  byte val = 0;
+  for(int i = 0; i < 8; i++)
+  {
+    val <<= 1;
+    val |= digitalRead(abacusPins[i]);
+  }
+  return val;
+}
